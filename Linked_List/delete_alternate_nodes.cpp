accd55aa00595cf3da51995b3c4eb7f127dc9c60
Dhruv patel <33346480+dhruvbpatel@users.noreply.github.com>
2020-01-07 19:27:16 +0530

    add new content in linked list section

---
+++ Linked_List/delete_alternate_nodes.cpp
@@ -0,0 +1,66 @@
+/*
+Delete alternate nodes
+
+
+Given a linked list, delete all alternate nodes in the list. That means given Linked List is -
+10 20 30 40 50 60 
+
+you need to delete nodes - 20, 40 and 60
+
+Note : Head of the list will remain same. Don't need to print or return anything.
+
+Input Constraints:
+1 <= Length of Linked List <=10^6.
+
+Input format :
+Linked list elements (separated by space and terminated by -1)
+
+Output Format :
+Updated linked list elements 
+
+Sample Input :
+
+1 2 3 4 5 -1
+
+Sample Output :
+
+1 3 5
+*/
+
+
+#include <bits/stdc++.h>
+
+void delete_alternate_node_LinkedList(Node *head) {
+    
+    
+    
+    Node *t1 = head;
+    Node *t2 = head->next;
+
+    if(head==NULL){
+        return;
+    }
+    
+    
+    while(t1!=NULL && t2!=NULL){
+        
+        Node *temp = t2;
+        t1->next = t2->next;
+        free(temp);
+        
+        t1 =t1->next;
+        
+            
+        
+        if(t1!=NULL){
+            t2 = t1->next;
+        }
+        
+        
+    }
+    
+   
+
+    
+    
+}
